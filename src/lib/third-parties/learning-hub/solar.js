57c6085f07b11750e01adefb59d4c1be538edd83
Vladimir Varbanov <v.varbanov@eos-matrix.bg>
2024-03-29 18:23:49 +0200

    Launching a extenal bundled app in Aurelia componante

--- src/lib/third-parties/learning-hub/solar.js
+++ src/lib/third-parties/learning-hub/solar.js
@@ -5,20 +5,48 @@ export class Solar {
     this.state = state;
   }
 
-  create() {
-    return customElements.define('solar-element', class extends HTMLElement {
+  createElement() {
+    customElements.define('solar-element', class extends HTMLElement {
       constructor(message = "Hola from solar") {
         super();
         this.message = message;
         this.attachShadow({mode: 'open'});
-        this.shadowRoot.innerHTML = `<p>${this.message}</p>`;
+        console.log(this.shadowRoot);
       }
     });
   }
 
+  addStyles() {
+    const styles = document.createElement('link');
+    styles.rel = 'stylesheet';
+    styles.href = 'http://localhost:8000/styles.css';
+    document.head.appendChild(styles);
+  }
+
+  createScripts() {
+    // create the scripts elements 
+    const main = document.createElement('script');
+    main.src = 'http://localhost:8000/main.js';
+    main.type = 'module';
+    main.onload = () => {
+      console.log('Solar loaded');
+    };
+    
+    const polyfill = document.createElement('script');
+    polyfill.src = 'http://localhost:8000/polyfills.js';
+    polyfill.onload = () => {
+      console.log('Polyfill loaded');
+    };
+
+    document.body.appendChild(polyfill);
+    document.body.appendChild(main);
+  }
+
   render() {
-    this.create();
-    const solarElement = document.createElement('solar-element');
+    this.addStyles();
+    this.createElement();
+    this.createScripts();
+    const solarElement = document.createElement('app-root');
     return solarElement;
   }
 }
