3d0e5b4a9afda68432a03a57337c334a2dfac34f
Vladimir Varbanov <v.varbanov@eos-matrix.bg>
2024-04-05 17:30:41 +0300

    Running in the shadow DOM

--- src/lib/third-parties/learning-hub/solar.js
+++ src/lib/third-parties/learning-hub/solar.js
@@ -4,52 +4,53 @@ export class Solar {
   }
 
   create() {
-    const host = document.createElement('div');
-    host.id = 'learning-hub';
-    host.classList.add('m3-learning-hub');
-    customElements.define('app-root', class extends HTMLElement {
-      constructor() {
-        super();
+    let template = document.createElement("template");
+    template.innerHTML = `
+      <style>
+      @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+      @import url('http://localhost:8000/styles.css');
+        :host {
+          display: block;
+          padding: 10px;
+          color: var(--x-foo-text-color, black);
+          background-color: var(--x-foo-background-color, white);
+        }
+        h1 {
+          color: red;
+        }
+      </style>
+      <link rel="stylesheets" href="http://localhost:8000/styles.css"></style>
+      <h1>Hello, Shadow DOM!</h1>
+    `;
+
+    customElements.define(
+      "learning-hub",
+      class extends HTMLElement {
+        constructor() {
+          super(); 
+          const shadowRoot = this.attachShadow({ mode: 'open' });
+          shadowRoot.appendChild(template.content.cloneNode(true));
+        }
+
+        connectedCallback() {
+          this.id = "learning-hub";
+          this.shadowRoot.appendChild(document.createElement("app-root"));
+          const s1 = document.createElement('script');
+          s1.type = 'module'
+          s1.src = 'http://localhost:8000/main.js'
+          this.shadowRoot.appendChild(s1);
+          const s2 = document.createElement('script');
+          s2.src = 'http://localhost:8000/polyfills.js';
+          this.shadowRoot.appendChild(s2);
+        }
       }
+    );
 
-      connectedCallback() {
-        console.log('Connected');
-        this.className = 'm3-app-root';	
-      }
-    });
-
-    host.appendChild(document.createElement('app-root'));
-
-    return host;
-  }
-
-  addStyles() {
-    const styles = document.createElement('link');
-    styles.rel = 'stylesheet';
-    styles.href = 'http://localhost:8000/styles.css';
-    document.head.appendChild(styles);
-  }
-
-  createScripts() {
-    const main = document.createElement('script');
-    main.src = 'http://localhost:8000/main.js';
-    main.type = 'module';
-    main.onload = () => {
-      console.log('Main loaded');
-    };
-    const polyfill = document.createElement('script');
-    polyfill.src = 'http://localhost:8000/polyfills.js';
-    polyfill.onload = () => {
-      console.log('Polyfill loaded');
-    };
-
-    document.body.appendChild(polyfill);
-    document.body.appendChild(main);
+    const appRoot = document.createElement("learning-hub");
+    return appRoot;
   }
 
   render() {
-    this.addStyles();
-    this.createScripts();
     return this.appRoot;
   }
 }
