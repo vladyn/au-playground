043f49bd487836ad30be981398ec401f4e7699e0
Vladimir Varbanov <v.varbanov@eos-matrix.bg>
2025-12-06 10:25:55 +0200

    Adjusts toaster UI and positioning logic
    Improves the toaster element's styling by reducing its maximum width
    and implements dynamic positioning to prevent overlapping when multiple
    toasters are active, ensuring a better user experience.

--- src/resources/elements/toaster/toaster.js
+++ src/resources/elements/toaster/toaster.js
@@ -7,6 +7,8 @@ export class Toaster {
   @bindable title = 'Toaster';
   @bindable message = 'This is a toaster message';
 
+  count = null;
+
   constructor(element, toasterController) {
     this.element = element;
     this.controller = toasterController;
@@ -15,6 +17,18 @@ export class Toaster {
   bind(bindingContext, overrideContext) {
     this.title = bindingContext?.title || this.title;
     this.message = bindingContext?.message || this.message;
+    this.count = overrideContext.bindingContext.controller.renderer.toasterControllers.length;
+    
+    if (this.count > 0) {
+      this.title += ` (${this.count})`;
+      const toastMessages = this.element.querySelectorAll('.toaster');
+      if (toastMessages?.length > 1) {
+        /// query this element and update the the top position with offset 160px per toast message
+        toastMessages.forEach((toastMessage, index) => {
+          toastMessage.style.top = `${index * 160 + 20}px`;
+        });
+      }
+    }
   }
 
   hide() {
